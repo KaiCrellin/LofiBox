ba4a0eb92d501feaad902880a811c88db4a3687b
Kai Crellin <KaiCrellin1244@gmail.com>
2025-07-28 13:52:35 +0100

    Lofi Box Start

---
+++ config/song_scan.php
@@ -0,0 +1,40 @@
+<?php
+
+$musicDir = __DIR__ . '/../Songs';
+$coverDir = __DIR__ . '/../covers';
+$allowedExtensions = ['mp3', 'wav', 'ogg'];
+
+
+$files = scandir($musicDir);
+
+
+foreach($files as $file) {
+    if (in_array(pathinfo($file, PATHINFO_EXTENSION), $allowedExtensions) === false) {
+        continue;
+    }
+
+    $filename = $file;
+    $basename = pathinfo($filename, PATHINFO_FILENAME);
+    $title = ucwords(str_replace(["-", "_"], ' ', $basename));
+    $artists = 'UNKNOWN'; // can improve later with ID3
+    $duration = null;
+    $cover = null;
+
+    $stmt = $pdo->prepare("SELECT COUNT(*) FROM songs WHERE filename = ?");
+    $stmt->execute([$filename]);
+    if ($stmt->fetchColumn() > 0 ) {
+        continue;
+    }
+
+
+    $possibleImage = $basename. '.jpg';
+    if (file_exists($coverDir . '/' . $possibleImage)) {
+        $cover = $possibleImage;
+    }
+
+    $insert = $pdo->prepare("INSERT INTO songs (filename, title, artist, duration, cover) VALUES (?,?,?,?,?) ");
+    $insert->execute([$filename,$title, $artists, $duration, $cover]);
+}
+
+
+?>
---
+++ lib/schema.sql
@@ -0,0 +1,33 @@
+CREATE DATABASE IF NOT EXISTS lofibox;
+USE lofibox;
+
+
+
+CREATE TABLE IF NOT EXISTS songs (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    filename VARCHAR(255) NOT NULL,
+    title VARCHAR(255),
+    artist VARCHAR(255),
+    duration INT,
+    cover VARCHAR(255) NOT NULL,
+    date_added DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+
+
+CREATE TABLE IF NOT EXISTS users  (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(255) UNIQUE,
+    email VARCHAR(255) UNIQUE,
+    password VARCHAR(255), 
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+
+CREATE TABLE IF NOT EXISTS favourites  (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT,
+    song_id INT,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (song_id) REFERENCES songs(id)
+);
